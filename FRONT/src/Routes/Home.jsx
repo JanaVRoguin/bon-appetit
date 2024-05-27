6ca05961d740b2b4644c035d8fc09f1732cc5f04
Exelh <exeg2014@gmail.com>
2024-05-26 22:07:12 -0300

    Fotos y tarjetas giratorias

--- FRONT/src/Routes/Home.jsx
+++ FRONT/src/Routes/Home.jsx
@@ -3,35 +3,35 @@ import { Link } from 'react-router-dom';
 import Recomendados from '../Components/Recomendados';
 import SearchBar from '../Components/SearchBar';
 
-
 export const Home = () => {
   const handleSearch = (term) => {
     console.log('Buscando recetas para:', term); // Aquí puedes implementar la lógica de búsqueda
   };
+
+  const categories = [
+    { path: '/desayuno', title: 'DESAYUNO', description: '"Comienza tu día con energía y vitalidad con nuestras recetas de desayuno. Desde opciones rápidas y saludables hasta platos más elaborados para los fines de semana, encontrarás una variedad de recetas que te ayudarán a empezar la mañana de la mejor manera."', image: '../../public/Images/Desayuno.jpeg' },
+    { path: '/almuerzo', title: 'ALMUERZO', description: '"Disfruta de un almuerzo delicioso y nutritivo con nuestras variadas recetas. Ya sea que busques platos ligeros para mantenerte activo durante el día o comidas sustanciosas que te llenen de energía, tenemos la receta perfecta para cada ocasión."', image: '../../public/Images/Almuerzo.jpeg' },
+    { path: '/mediatarde', title: 'MERIENDA', description: '"Recarga energías a media tarde con nuestras deliciosas recetas de merienda. Encuentra desde snacks saludables y fáciles de preparar hasta dulces tentaciones que te alegrarán el día. Perfectas para disfrutar solo o compartir con amigos y familia."', image: '../../public/Images/Merienda.jpg' },
+    { path: '/cena', title: 'CENA', description: '"Termina tu día con una cena reconfortante y sabrosa. Nuestras recetas de cena están diseñadas para ser nutritivas y deliciosas, ofreciendo una variedad de opciones que te ayudarán a relajarte y disfrutar de una comida completa antes de descansar."', image: '../../public/Images/Cena.jpeg' },
+  ];
+
   return (
-    
-    
     <div className="home">
       <SearchBar onSearch={handleSearch}/>
       <div className="card-container">
-          {/* <h2></h2> */}
-          <Link to="/desayuno" className="card"> {/* Navega a la página de desayuno */}
-            <h1>Desayuno</h1>
+        {categories.map((category, index) => (
+          <Link to={category.path} className="card" key={index}>
+            <div className="card-front" style={{ backgroundImage: `url(${category.image})` }}>
+              <h1>{category.title}</h1>
+            </div>
+            <div className="card-back">
+              <p>{category.description}</p>
+            </div>
           </Link>
-          <Link to="/almuerzo" className="card"> {/* Navega a la página de almuerzo */}
-            <h1>Almuerzo</h1>
-          </Link>
-          <Link to="/mediatarde" className="card"> {/* Navega a la página de mediatarde */}
-            <h1>Mediatarde</h1>
-          </Link>
-          <Link to="/cena" className="card"> {/* Navega a la página de cena */}
-            <h1>Cena</h1>
-          </Link>
-
+        ))}
       </div>
       <div>
-          
-          <Recomendados/>
+        <Recomendados/>
       </div>
     </div>
   );
