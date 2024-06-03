b03e1b088e081473c6cc56745957e2325620d400
Francisco Heredia <flheredia22@gmail.com>
2024-06-03 19:36:17 -0300

    Se agrego la logica de MyAccount

--- FRONT/src/Components/MyAccount.jsx
+++ FRONT/src/Components/MyAccount.jsx
@@ -1,23 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { BASE_URL } from '../utils/config';
 import axios from 'axios';
 
 const MyAccount = () => {
-    const userHardcodeado = {
-        nombre: 'Wilfren',
-        apellido: 'Pereira',
-        correo: JSON.parse(localStorage.getItem('email'))
-    }
-    const {nombre, apellido, correo} = userHardcodeado;
+  const [user, setUser] = useState({
+    nombre: '',
+    apellido: '',
+    correo: JSON.parse(localStorage.getItem('email'))
+  });
+    const {nombre, apellido, correo} = user;
     const url = `${BASE_URL}usuarios/buscar/${correo}`;
-    // console.log(JSON.parse(localStorage.getItem('email')));
     
     useEffect(() => {
-      axios(url)
-      .then((response) => {
-        console.log(response.data);
-      })
-    }, []) // Preguntarle a Fran
+      const fetchData = async () => {
+        try {
+          const token = JSON.parse(localStorage.getItem('token'));
+          const response = await axios.get(url, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json'
+            }
+          });
+          
+          setUser({
+            ...user,
+            nombre: response.data.nombre,
+            apellido: response.data.apellido
+          });
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+      fetchData();
+    }, [url, user]); 
     
   return (
     <>
@@ -25,7 +40,9 @@ const MyAccount = () => {
         <h2>Mis datos</h2>
         <div>
             <h3>Nombre:</h3>
-            <p>{`${nombre} ${apellido}`}</p>
+            <p>{`${nombre}`}</p>
+            <h3>Apellido:</h3>
+            <p>{`${apellido}`}</p>
             <h3>Correo:</h3>
             <p>{`${correo}`}</p>
         </div>
