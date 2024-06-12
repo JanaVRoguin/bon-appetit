bca067cc19b1a8d3cba9b389ce290ff77152ec49
Exelh <exeg2014@gmail.com>
2024-06-11 22:59:28 -0300

    SEARCHBAR FUNCIONANDO

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -3,16 +3,19 @@ import { AuthProvider } from './Context/Auth/AuthContext';
 import { ContextProvider } from './Context';
 import { Footer, Navbar } from './Layouts';
 import './App.css'
+import { SearchProvider } from './Context/SearchContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <ContextProvider>
-        <Navbar/>
-        <AppRouter/>
-        <Footer/>
-      </ContextProvider>
-    </AuthProvider>
+    <SearchProvider>
+      <AuthProvider>
+        <ContextProvider>
+          <Navbar/>
+          <AppRouter/>
+          <Footer/>
+        </ContextProvider>
+      </AuthProvider>
+    </SearchProvider>
   )
 }
 
