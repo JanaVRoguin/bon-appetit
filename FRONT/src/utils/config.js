e405fa44b244647ba3d9eef7691d697ca99bd9af
stefanoluzi <stefanoluzi@hotmail.com>
2024-06-24 19:56:44 -0300

    pipeline dev

--- FRONT/src/utils/config.js
+++ FRONT/src/utils/config.js
@@ -1,2 +1,3 @@
 //URL base de la API back
-export const BASE_URL = import.meta.env.VITE_API_URL;
+// export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_URL = "http://localhost:8080";
